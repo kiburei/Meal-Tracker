a4da45ca872777eadff2c447059a168f385fcc55
kiburei <akiburei@gmail.com>
2017-06-01 20:11:29 +0300

    configure ts files

---
+++ app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MealTrackerComponent } from './meal-tracker.component';
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [MealTrackerComponent],
+  bootstrap: [MealTrackerComponent]
+})
+
+export class AppModule { }
---
+++ app/meal-tracker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'meal-tracker',
+  template: `
+      <h1>Meal Tracker</h1>
+  `
+})
+
+export class MealTrackerComponent {
+  
+}
